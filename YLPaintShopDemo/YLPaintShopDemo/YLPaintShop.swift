dcab89f46489cf47771beeae6283721a3faffd77
Yuhui Li <a@yuhuili.com>
2016-11-20 01:35:49 -0500

    Add paintEdge

--- YLPaintShopDemo/YLPaintShopDemo/YLPaintShop.swift
+++ YLPaintShopDemo/YLPaintShopDemo/YLPaintShop.swift
@@ -102,4 +102,89 @@ extension UIImage {
         
         return UIImage(cgImage: imageRef!)
     }
+    
+    func paintEdge(_ threshold: Int) -> UIImage? {
+        
+        let inImage:CGImage = self.cgImage!
+        let context = self.createContext(inImage)
+        
+        let width = inImage.width
+        let height = inImage.height
+        let total = width*height
+        
+        let rect = CGRect(x:0, y:0, width:width, height:height)
+        
+        context.clear(rect)
+        
+        context.draw(inImage, in: rect)
+        
+        let data = context.data
+        let dataType = UnsafeMutableRawPointer(data)!.assumingMemoryBound(to: UInt8.self)
+        
+        
+        for i in 0...total-1 {
+            // Find out which row which column currently is
+            let row:Int = i/width
+            let col:Int = i%width
+            let offset = 4 * i
+            
+            var isEdge = false;
+            
+            // Check all 8 pixels around it
+            for j in -1...1 {
+                var shouldExit = false
+                
+                for k in -1...1 {
+                    let newRow = row+j
+                    let newCol = col+k
+                    
+                    if newRow<0 || newRow>=height {
+                        continue
+                    } else if newCol<0 || newCol>=width {
+                        continue
+                    }
+                    
+                    let newOffset = 4 * ((width * newRow) + newCol)
+                    
+                    // Check if rgb diff is > threshold
+                    
+                    // First value (newOffset+0 and offset+0) is alpha, ignored
+                    if (Float(dataType[newOffset+1]) - Float(dataType[offset+1])) > Float(threshold) ||
+                        (Float(dataType[newOffset+2]) - Float(dataType[offset+2])) > Float(threshold) ||
+                        (Float(dataType[newOffset+3]) - Float(dataType[offset+3])) > Float(threshold){
+                        isEdge = true
+                        shouldExit = true
+                        break
+                    }
+                    
+                }
+                if shouldExit {
+                    break
+                }
+            }
+            
+            if isEdge {
+                // Paint black
+                dataType[offset] = 255
+                dataType[offset+1] = 0
+                dataType[offset+2] = 0
+                dataType[offset+3] = 0
+            } else {
+                // Paint white
+                dataType[offset] = 255
+                dataType[offset+1] = 255
+                dataType[offset+2] = 255
+                dataType[offset+3] = 255
+            }
+            
+        }
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapBytesPerRow = width * 4
+        
+        let finalContext = CGContext(data: data, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bitmapBytesPerRow, space: colorSpace, bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let imageRef = finalContext?.makeImage()
+        
+        return UIImage(cgImage: imageRef!)
+    }
 }
