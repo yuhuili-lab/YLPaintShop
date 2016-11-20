385d454c3e391bbcf4cbd9d7ea1642093fec2826
Yuhui Li <a@yuhuili.com>
2016-11-20 00:46:11 -0500

    Prevent repeated drawings

--- YLPaintShopDemo/YLPaintShopDemo/ViewController.swift
+++ YLPaintShopDemo/YLPaintShopDemo/ViewController.swift
@@ -16,20 +16,26 @@ class ViewController: UIViewController {
     // Put a delay to image render
     var shouldDraw = true
     var sampleImage = UIImage(named: "pusheen.jpg")!
+    var lastRadius = 20
+    var delay = 0.05
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        mainImageView.image = sampleImage.scatter(20)
+        slider.value = Float(lastRadius)
+        mainImageView.image = sampleImage.scatter(lastRadius)
         
     }
     
     @IBAction func sliderValueChanged(_ sender: AnyObject) {
-        if (shouldDraw) {
-            shouldDraw = false;
+        
+        // Add lastRadius to prevent repeated drawing of same integer radius
+        if shouldDraw && lastRadius != Int(slider.value) {
+            shouldDraw = false
+            lastRadius = Int(slider.value)
             
-            let dispatchTime: DispatchTime = DispatchTime.now() + Double(Int64(0.05 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+            let dispatchTime: DispatchTime = DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
             DispatchQueue.main.asyncAfter(deadline: dispatchTime, execute: {
                 self.shouldDraw = true;
             })
